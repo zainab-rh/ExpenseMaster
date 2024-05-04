460900b3cc9c9ba53eb2e5ef54cf0fe1bd664e15
Hectorfr <hector.freard@gmail.com>
2024-05-05 01:17:35 +0200

    Wrapper created with simple methods

---
+++ src/main/java/com/l22e11/helper/AccountWrapper.java
@@ -0,0 +1,42 @@
+package com.l22e11.helper;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import javafx.scene.image.Image;
+import model.Acount;
+
+public class AccountWrapper {
+    
+    // Wrapper call to the createUser from Acount API
+    // Return values ->
+    //  1 correct registration
+    //  0 incorrect registration
+    // -1 AcountDAO
+    public static int createAccountUser (String name,
+    String surname, String email, String login,
+    String password, Image image, LocalDate date ) {
+        try{
+            boolean isOk = Acount.getInstance().registerUser(name, surname, email, login, password, image, date);
+            if (isOk) return 1;
+            else return 0;
+            
+        }catch (Exception AcountDAOException){return -1;}
+    }
+
+    // Wrapper call to the LogIn from Acount API
+    public static boolean logInUser ( String name, String password){
+        try{
+            boolean isOK = Acount.getInstance().logInUserByCredentials(name, password);
+
+            return isOK;
+        }catch (Exception AcountDAOException){ return false;}
+    }
+
+    // Wrapper call to the LogIn from Acount API
+    public static boolean logOut (){
+        try{
+            boolean isOK = Acount.getInstance().logOutUser();
+            return isOK;
+        }catch (Exception AcountDAOException){ return false;}
+    } 
+}
