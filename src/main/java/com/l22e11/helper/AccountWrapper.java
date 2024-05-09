bea24aaf58472f3235501c3bbca6c8ba17084b5b
Zainab Rehman <zainabrehman466@gmail.com>
2024-05-09 22:08:34 +0200

    Wrapper class finished

--- src/main/java/com/l22e11/helper/AccountWrapper.java
+++ src/main/java/com/l22e11/helper/AccountWrapper.java
@@ -1,6 +1,8 @@
 package com.l22e11.helper;
 
 import java.time.LocalDate;
+import java.util.List;
+
 import javafx.scene.image.Image;
 import model.Acount;
 import model.Category;
@@ -14,33 +16,42 @@ public class AccountWrapper {
     //  1 correct registration
     //  0 incorrect registration
     // -1 AcountDAO
-    public static int createAccountUser (String name,
-    String surname, String email, String login,
-    String password, Image image, LocalDate date ) {
-        try{
+
+
+    // Method to register a new user
+    public int registerUser(String name, String surname, String email, String login, String password, Image image, LocalDate date) {
+        try {
             boolean isOk = Acount.getInstance().registerUser(name, surname, email, login, password, image, date);
             if (isOk) return 1;
             else return 0;
-            
-        }catch (Exception AcountDAOException){return -1;}
+        } catch (Exception AcountDAOException) {
+            return -1;
+        }
+    }
+ // Wrapper call to the LogIn method from the Acount API
+    public boolean logInUserByCredentials(String
+    login, String password)
+    {
+        try {
+            boolean isOk = Acount.getInstance().logInUserByCredentials(login, password);
+            return isOk;
+        } catch (Exception AcountDAOException) {
+            return false;
+        }
     }
 
-    // Wrapper call to the LogIn from Acount API
-    public static boolean logInUser ( String name, String password){
-        try{
-            boolean isOK = Acount.getInstance().logInUserByCredentials(name, password);
-
-            return isOK;
-        }catch (Exception AcountDAOException){ return false;}
+   
+   
+    // Wrapper call to the Logout method from the Acount API
+    public boolean logOutUser() {
+        try {
+            boolean isOk = Acount.getInstance().logOutUser();
+            return isOk;
+        } catch (Exception AcountDAOException) {
+            return false;
+        }
     }
 
-    // Wrapper call to the LogIn from Acount API
-    public static boolean logOut (){
-        try{
-            boolean isOK = Acount.getInstance().logOutUser();
-            return isOK;
-        }catch (Exception AcountDAOException){ return false;}
-    } 
     // Method to register a new category
     public static boolean registerCategory(String name, String description){
         try{
@@ -58,6 +69,8 @@ public class AccountWrapper {
                 return false;
         }
     } 
+
+    // Method to register a new charge
     public static boolean registerCharge(String name, String description, double cost, int units, Image scanImage, LocalDate date, Category category){
         try{
             boolean isOK = Acount.getInstance().registerCharge(name, description, cost, units, scanImage, date, category);
@@ -65,12 +78,21 @@ public class AccountWrapper {
         }catch (Exception AcountDAOException){ return false;}
     }   
 
-    public static boolean removeCharge(Charge charge) {
+  // Method to get the user's categories
+    public List<Category> getUserCategories() {
         try {
-                boolean isOK = Acount.getInstance().removeCharge(charge);
-                return isOK;
+            return Acount.getInstance().getUserCategories();
         } catch (Exception AcountDAOException) {
-                return false;
+            return null;
+        }
+    }
+
+    // Method to get the user's charges
+    public List<Charge> getUserCharges() {
+        try {
+            return Acount.getInstance().getUserCharges();
+        } catch (Exception AcountDAOException) {
+            return null;
         }
     }
 }
