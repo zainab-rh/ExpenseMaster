4dfbe6f1317925d1cf919e37d0e9df2f58443ee2
camelCase1492 <rubennieto04@gmail.com>
2024-06-12 16:11:31 +0200

    Bugs

--- src/main/java/com/l22e11/helper/ExpenseFieldValidation.java
+++ src/main/java/com/l22e11/helper/ExpenseFieldValidation.java
@@ -269,7 +269,7 @@ public class ExpenseFieldValidation {
 	public static boolean registerOrUpdateExpense() {
 		String name = expenseBoxes[EXPENSE_NAME_IDX].getText();
 		String description = expenseBoxes[EXPENSE_DESCRIPTION_IDX].getText();
-		double cost = Double.parseDouble(expenseBoxes[EXPENSE_COST_IDX].getText());
+		double cost = Double.parseDouble(expenseBoxes[EXPENSE_COST_IDX].getText().replaceAll(",", "."));
 		Category category = Utils.getCategoryByName(expenseCategory.getValue());
 		int unit = Integer.parseInt(expenseBoxes[EXPENSE_UNIT_IDX].getText());
 		LocalDate date = expenseDate.getValue();
